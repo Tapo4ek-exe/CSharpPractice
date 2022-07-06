095c92ba5200baa292fd735933a80d70df53f58f
Tapo4ek-exe <am14102001@gmail.com>
2022-07-06 17:20:24 +0300

    - Добавлен файл .gitignore - Добавлен проект по первому заданию - "Консольное приложение, вычисляющее квадратный корень из введенного числа"

---
+++ SquareRootCalculating/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод данных пользователем
+            Console.WriteLine("Введите число для извлечения корня: ");
+            double number;
+            string? input = Console.ReadLine();
+            bool isConverted = double.TryParse(input, out number);
+
+            /// Проверка успеха конвертации
+            if (!isConverted)
+            {
+                Console.WriteLine($"Ошибка: {input} не является числом!");
+                return;
+            }
+
+            /// Проверка числа на отрицательность
+            if (number < 0)
+            {
+                Console.WriteLine("Ошибка: корень отрицательного числа вычислить невозможно!");
+            }
+
+            double result = Math.Sqrt(number);
+
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
