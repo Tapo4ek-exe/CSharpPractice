c6667251b807f2d7a9261b72e0b4d101590afe49
Tapo4ek-exe <am14102001@gmail.com>
2022-07-06 17:46:43 +0300

    - Добавлен проект для задания №2 - "Вычисление среднего арифметического трех чисел"

---
+++ ArithmeticalMeanCalculating/CustomMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticalMeanCalculating
+{
+    static class CustomMath
+    {
+        // Функция для подсчета среднего арифметического чисел
+        public static double Average(params double[] numbers)
+        {
+            // Подсчет суммы чисел
+            double sum = 0;
+            foreach (double number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum / numbers.Length;
+        }
+    }
+}
---
+++ ArithmeticalMeanCalculating/Program.cs
@@ -0,0 +1,26 @@
+﻿using ArithmeticalMeanCalculating;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод данных пользователем
+        Console.WriteLine("Введите 3 числа: ");
+        double[] numbers = new double[3];
+        for (int index = 0; index < numbers.Length; index++)
+        {
+            string? input = Console.ReadLine();
+            bool isConverted = double.TryParse(input, out numbers[index]);
+            
+            /// Проверка успеха конвертации
+            if (!isConverted)
+            {
+                Console.WriteLine($"Ошибка: {input} не является числом!");
+                return;
+            }
+        }
+
+        double result = CustomMath.Average(numbers);
+        Console.WriteLine($"Среднее арифметическое: {result}");
+    }
+}
