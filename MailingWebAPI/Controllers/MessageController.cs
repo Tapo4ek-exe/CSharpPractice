a827d1331deb9b8aa68672d685c0bd2fc4aaee5c
Tapo4ek-exe <am14102001@gmail.com>
2022-07-18 23:10:20 +0300

    - Добавлено логгирование запросов - Добавлено описание методов Swagger

--- MailingWebAPI/Controllers/MessageController.cs
+++ MailingWebAPI/Controllers/MessageController.cs
@@ -12,12 +12,32 @@ namespace MailingWebAPI.Controllers
 
         private readonly ILogger<MessageController> _logger;
 
+
         public MessageController(Dictionary<string, int> db, ILogger<MessageController> logger)
         {
             _db = db;
             _logger = logger;
         }
 
+
+        /// <summary>
+        /// Sends message to emails
+        /// </summary>
+        /// <param name="messageToEmails">MessageToEmails object</param>
+        /// <remarks>
+        /// Sample request:
+        /// POST api/messages/send
+        ///{
+        ///     "emails": [
+        ///         "test1@email.com",
+        ///         "test2@email.com"
+        ///     ],
+        ///     "message": "Hello!"
+        ///}
+        /// </remarks>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
+        /// <response code="400">If the messageToEmails is invalid</response>
         [HttpPost("send")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -26,9 +46,12 @@ namespace MailingWebAPI.Controllers
             EmailAddressAttribute emailValidator = new EmailAddressAttribute();
             foreach (var email in messageToEmails.Emails)
             {
+                _logger.LogInformation($"Processing request {Request.Path}");
+
                 //  Проверка корректности email адреса
                 if (!emailValidator.IsValid(email))
                 {
+                    _logger.LogWarning($"Request {Request.Path}: invalid email");
                     ModelState.AddModelError("error", $"{email} is not a valid e-mail address.");
                     return BadRequest(ModelState);
                 }
@@ -45,31 +68,59 @@ namespace MailingWebAPI.Controllers
                     _db.Add(email, 1);
                 }
             }
-
+            _logger.LogInformation($"Request {Request.Path} passed");
             return Ok();
         }
 
 
+        /// <summary>
+        /// Gets the stats of each email
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// GET api/messages/stat
+        /// </remarks>
+        /// <returns>Stats of each email</returns>
+        /// <response code="200">Success</response>
         [HttpGet("stat")]
         public async Task<ActionResult> GetStat()
         {
+            _logger.LogInformation($"Processing request {Request.Path}");
+
             return Ok(_db.Select(kvp => new EmailStat { Email = kvp.Key, MessageCount = kvp.Value }).ToList());
         }
 
 
+        /// <summary>
+        /// Gets the number of messages sent to a given email
+        /// </summary>
+        /// <param name="email">Email for which the number of messages is counted</param>
+        /// <remarks>
+        /// Sample request:
+        /// GET api/messages/count/test1@email.com
+        /// </remarks>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
+        /// <response code="400">If the email is invalid</response>
+        /// <response code="404">If the email not found</response>
         [HttpGet("count/{email}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> GetMessageCount([EmailAddress] string email)
         {
+            _logger.LogInformation($"Processing request {Request.Path}");
+
             int count;
             bool isSuccess = _db.TryGetValue(email, out count);
             if (isSuccess)
             {
+                _logger.LogInformation($"Request {Request.Path} passed");
                 return Ok(count);
             }
             else
             {
+                _logger.LogWarning($"Request {Request.Path}: email not found");
                 return NotFound();
             }
         }
--- MailingWebAPI/Program.cs
+++ MailingWebAPI/Program.cs
@@ -1,16 +1,29 @@
+using System;
+using System.IO;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    config.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddSingleton(typeof(Dictionary<string, int>));
 
 var app = builder.Build();
 
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(config =>
+{
+    config.RoutePrefix = String.Empty;
+    config.SwaggerEndpoint("swagger/v1/swagger.json", "Mailing API");
+});
 
 app.UseHttpsRedirection();
 
