c6352eb50b1577ad519ca57d60be0c5cc65d66d0
Tapo4ek-exe <am14102001@gmail.com>
2022-07-18 18:02:29 +0300

    - Внедрено кэширование Redis - Добавлен функционал вывода всех записей в БД - Добавлен функционал получения количества игроков и суммарного расхода на игроков - Изменена структура консольного приложения

--- FootballData/Program.cs
+++ FootballData/Program.cs
@@ -6,35 +6,41 @@ namespace FootballData
     class Program
     {
         private static string commands = "\nadd [player|team|playerInTeam] - добавление новых данных о игроке/команде/игроке в команде\n" +
+                                         "get [playerCount|totalExpenses] - получение информации о количестве игроков/суммарных расходах на игроков\n" +
+                                         "info - выводит информацию о имеющихся записях\n" +
                                          "exit - выход из программы\n";
 
 
         static void Main(string[] args)
         {
-            PrintHeader();
-            using (var db = new FootballDbContext())
+            try
             {
-                while (true)
+                // Подключение к Redis
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+                IDatabase redisDb = redis.GetDatabase();
+
+                PrintHeader();
+                using (var db = new FootballDbContext())
                 {
-                    Console.Write("> ");
-                    string? command = Console.ReadLine();
-
-                    if (String.IsNullOrEmpty(command)) continue;
-
-                    string[] commandParts = command.Split(" ");
-
-                    if (commandParts[0] == "exit") break;
-
-                    else if (commandParts[0] == "help") Console.WriteLine(commands);
-
-                    else if (commandParts.Length < 2)
+                    while (true)
                     {
-                        Console.WriteLine("Команда не найдена!");
-                    }
+                        // Ввод команды
+                        Console.Write("> ");
+                        string? command = Console.ReadLine();
+                        if (String.IsNullOrEmpty(command)) continue;
 
-                    else if (commandParts[0] == "add")
-                    {
-                        if (commandParts[1] == "player")
+                        // Определение и выполнение команды
+                        string[] commandParts = command.Split(" ");
+                        if (commandParts[0] == "exit") break;   // выход из программы
+                        else if (commandParts[0] == "help")     // вывод списка доступных команд
+                            Console.WriteLine(commands);
+                        else if (commandParts[0] == "info")     // вывод информации о всех записях в бд
+                        {
+                            PrintAllPlayers(db);
+                            PrintAllTeams(db);
+                            PrintAllPlayersInTeams(db);
+                        }
+                        else if (commandParts.Length >= 2 && commandParts[0] == "add" && commandParts[1] == "player")   // добавление игрока
                         {
                             // Ввод данных об игроке
                             // Ввод имени игрока
@@ -50,8 +56,10 @@ namespace FootballData
                             var player = db.Players.Add(new Player { Name = name, Age = age });
                             db.SaveChanges();
                             Console.WriteLine($"Id нового игрока: {player.Entity.Id}");
+
+                            redisDb.StringIncrement("playerCount");
                         }
-                        else if (commandParts[1] == "team")
+                        else if (commandParts.Length >= 2 && commandParts[0] == "add" && commandParts[1] == "team")     // добавление команды
                         {
                             // Ввод данных о команде
                             Console.WriteLine("Введите название команды:");
@@ -62,7 +70,7 @@ namespace FootballData
                             db.SaveChanges();
                             Console.WriteLine($"Id новой команды: {team.Entity.Id}");
                         }
-                        else if (commandParts[1] == "playerInTeam")
+                        else if (commandParts.Length >= 2 && commandParts[0] == "add" && commandParts[1] == "playerInTeam")     // добавление игрока в команду
                         {
                             // Ввод Id игрока
                             PrintAllPlayers(db);
@@ -85,21 +93,42 @@ namespace FootballData
                             var playerInTeam = db.PlayersInTeams.Add(new PlayersInTeams { PlayerId = playerId, TeamId = teamId, Salary = salary });
                             db.SaveChanges();
                             Console.WriteLine($"Игрок #{playerInTeam.Entity.PlayerId} добавлен в команду #{playerInTeam.Entity.TeamId}");
+
+                            redisDb.StringIncrement("totalExpenses", salary);
+                        }
+                        else if (commandParts.Length >= 2 && commandParts[0] == "get" && commandParts[1] == "playerCount")      // вывод общего кол-ва игроков
+                        {
+                            string? value = redisDb.StringGet("playerCount");
+                            Console.WriteLine($"Общее кол-во игроков: {value ?? "0"}");
+                        }
+                        else if (commandParts.Length >= 2 && commandParts[0] == "get" && commandParts[1] == "totalExpenses")    // вывод суммарных расходов на игроков
+                        {
+                            string? value = redisDb.StringGet("totalExpenses");
+                            Console.WriteLine($"Суммарные расходы на игроков: {value ?? "0"}");
                         }
                         else
                         {
-                            Console.WriteLine("Неверные аргументы для команды add!");
+                            Console.WriteLine("Команда не распознана!");
                         }
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("Команда не распознана!");
                     }
                 }
             }
-
+            catch (Npgsql.PostgresException)
+            {
+                Console.WriteLine("Не удалось подключиться к PostgreSQL!");
+                return;
+            }
+            catch (RedisException)
+            {
+                Console.WriteLine("Не удалось подключиться к Redis Server!");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+
         private static void PrintHeader()
         {
             Console.WriteLine("УПРАВЛЕНИЕ ДАННЫМИ О ФУТБОЛЬНЫХ КОМАНДАХ И ИГРОКАХ");
@@ -118,10 +147,19 @@ namespace FootballData
 
         private static void PrintAllTeams(FootballDbContext db)
         {
-            Console.WriteLine("Список игроков:");
+            Console.WriteLine("Список команд:");
             foreach (var team in db.Teams)
             {
                 Console.WriteLine($"\t Id - {team.Id}, Name - {team.Name}");
+            }
+        }
+
+        private static void PrintAllPlayersInTeams(FootballDbContext db)
+        {
+            Console.WriteLine("Список игроков в командах:");
+            foreach (var playerInTeam in db.PlayersInTeams)
+            {
+                Console.WriteLine($"\t Игрок #{playerInTeam.PlayerId} находится в команде #{playerInTeam.TeamId} с зарплатой {playerInTeam.Salary}");
             }
         }
     }
